6abb7070c54b2924c29727a18d7c2b5db156c374
basketball <123@qq.com>
2020-08-29 10:07:57 +0800

    zongben

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2020-06-08 14:04:04
+* @Last Modified by:   Administrator
+* @Last Modified time: 2020-06-15 09:58:26
+*/
+
+$('.bannerbox .right_box right .')
